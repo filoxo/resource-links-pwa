fccf438dd4f357264d41f4e1da7063d386db856c
Carlos Filoteo <c.filoteo@live.com>
2017-07-16 22:38:16 -0600

    Convert header to sfc

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,14 @@
 import { h, Component } from "preact";
 import style from "./style";
 
-export default class Header extends Component {
-  render() {
-    return (
-      <header class={style.header}>
-        <h1>
-          {this.props.title}
-        </h1>
-      </header>
-    );
-  }
-}
+const Header = props => {
+  return (
+    <header class={style.header}>
+      <h1>
+        {props.title}
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
