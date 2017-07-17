7c35b45e9dcc316eadf15dec9ff42ed3afe2ba52
Carlos Filoteo <c.filoteo@live.com>
2017-07-16 22:40:55 -0600

    Add test layout using new components

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,22 @@
 import { h, Component } from "preact";
 
 import Header from "./header";
+import Section from "./section";
+import ResourceLink from "./resourceLink";
+import Layout from "./layout";
 
 export default class App extends Component {
   render() {
     return (
       <div id="app">
         <Header title="Resource Links" />
+        <Layout>
+          <Section>
+            <ResourceLink url={"https://test.com"} heading={"Wow such title"} />
+            <ResourceLink url={"https://test.com"} heading={"Wow such title"} />
+            <ResourceLink url={"https://test.com"} heading={"Wow such title"} />
+          </Section>
+        </Layout>
       </div>
     );
   }
