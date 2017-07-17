230c230f502edda71c6c61ca68074b54f8bc0c9e
Carlos Filoteo <c.filoteo@live.com>
2017-07-16 22:38:42 -0600

    Add ResourceLink component

---
+++ src/components/resourceLink/index.js
@@ -0,0 +1,25 @@
+import { h, Component } from "preact";
+import style from "./style";
+
+const ResourceLink = ({ url, heading, description }) => {
+  return (
+    <div class={style.resource}>
+      <a class={style.url} href={url} target="_blank" rel="noreferrer">
+        {heading}
+      </a>
+      <div class={style.printMetadata}>
+        <h2>
+          {heading}
+        </h2>
+        <p>
+          {url}
+        </p>
+      </div>
+      <p>
+        {description}
+      </p>
+    </div>
+  );
+};
+
+export default ResourceLink;
