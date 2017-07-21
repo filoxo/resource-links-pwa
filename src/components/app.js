a57c7a0aecfc1f1cefc83dd7a2497915d64515f8
Carlos Filoteo <c.filoteo@live.com>
2017-07-21 10:02:24 -0600

    Add search functionality

--- src/components/app.js
+++ src/components/app.js
@@ -5,21 +5,46 @@ import Section from "./section";
 import ResourceLink from "./resourceLink";
 import Layout from "./layout";
 
-import { sections } from "./data.json";
+import { sections as sectionsSrc } from "./data.json";
 
 export default class App extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { sections: this.getSectionSrc() };
+    this.handleOnInput = this.handleOnInput.bind(this);
+  }
+  getSectionSrc = () => JSON.parse(JSON.stringify(sectionsSrc));
+  handleOnInput($event) {
+    const term = $event.target.value;
+    if (term !== "") {
+      let filteredSections = this.state.sections.filter(section => {
+        section.links = section.links.filter(
+          ({ url, heading, description }) => {
+            const termReg = new RegExp(term, "gi");
+            return (
+              url.match(termReg) ||
+              heading.match(termReg) ||
+              description.match(termReg)
+            );
+          }
+        );
+        return section.links.length > 0;
+      });
+      this.setState({ sections: filteredSections });
+    } else {
+      this.setState({ sections: this.getSectionSrc() });
+    }
+  }
+  render({}, { sections }) {
     return (
       <div id="app">
-        <Header title="Resource Links" />
+        <Header title="Resource Links" filter={this.handleOnInput} />
         <Layout>
-            {sections.map(({ heading, links }) => {
-              return (
-                <Section heading={heading}>
-                  {links.map(link => <ResourceLink {...link} />)}
-                </Section>
-              );
-            })}
+          {sections.map(({ heading, links }) =>
+            <Section heading={heading}>
+              {links.map(link => <ResourceLink {...link} />)}
+            </Section>
+          )}
         </Layout>
       </div>
     );
